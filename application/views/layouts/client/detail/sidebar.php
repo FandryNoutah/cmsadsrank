ea10c561eb39f701decb1da27e12c9c44448b573
Miora <razakatiana.antenaina@yahoo.com>
2025-08-13 09:06:01 +0200

    acceuil modifier

--- application/views/layouts/client/detail/sidebar.php
+++ application/views/layouts/client/detail/sidebar.php
@@ -26,13 +26,13 @@
 			<li class="nav-item rounded">
 				<a class="nav-link text-secondary" href="<?= base_url('Client') ?>">
 					<img class="mr-2" src="<?= base_url('assets/images/icons/figma/icon-clipboardtext.svg') ?>" />
-					<span>Google Ads</span>
+					<span>Aircall</span>
 				</a>
 			</li>
 			<li class="nav-item rounded <?= ($this->uri->segment(1) == "Task") ? 'bg-white' : ''; ?>">
 				<a class="nav-link text-secondary" href="<?= base_url('Task') ?>">
 					<img class="mr-2" src="<?= base_url('assets/images/icons/figma/icon-clipboardtext.svg') ?>" />
-					<span>Tasks</span>
+					<span>Point bilan</span>
 				</a>
 			</li>
 		</ul>
@@ -50,7 +50,7 @@
 			<li class="nav-item rounded">
 				<a class="nav-link text-secondary" href="#">
 					<img class="mr-2" src="<?= base_url('assets/images/icons/figma/addressbook.svg') ?>" />
-					<span>Billing</span>
+					<span>Discussions</span>
 				</a>
 			</li>
 		</ul>
