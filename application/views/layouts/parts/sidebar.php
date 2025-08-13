2c9f49999f3041dfe98a6ec30b7a4be73f73ba4f
Miora <razakatiana.antenaina@yahoo.com>
2025-08-13 18:31:30 +0200

    note

--- application/views/layouts/parts/sidebar.php
+++ application/views/layouts/parts/sidebar.php
@@ -30,7 +30,7 @@
 				</a>
 			</li>
 			<li class="nav-item rounded">
-				<a class="nav-link text-secondary" href="<?= base_url('Note') ?>">
+				<a class="nav-link text-secondary" href="<?= base_url('Notes') ?>">
 					<img class="mr-2" src="<?= base_url('assets/images/icons/figma/icon-noteblank.svg') ?>" />
 					<span class="nav-label">Notes</span>
 				</a>
