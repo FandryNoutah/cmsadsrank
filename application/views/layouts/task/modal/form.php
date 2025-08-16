73fb4a13af316c7cec0caf4589e2b5fed7353d5a
RANDRIANJAFINJOHARY Fandresena Notahiana <fandresena.randrianjafinjohary@cpp-system.com>
2025-08-16 08:26:08 +0300

    Finished task note modal
    Former-commit-id: d5bf159b5033f30df4ccd6c81a9643836ea0eed0

--- application/views/layouts/task/modal/form.php
+++ application/views/layouts/task/modal/form.php
@@ -11,40 +11,47 @@
 				</div>
 				<div class="modal-body">
 
-					<div class="form-group">
-						<label for="demand_date">Date de la demande</label>
-						<input type="date" name="demand_date" id="demand_date" class="form-control">
+					<div class="form-row">
+						<div class="col form-group">
+							<label for="task_type">Type</label>
+							<select name="task_type" id="task_type" class="form-control">
+								<option value="information">Information</option>
+								<option value="tache">Tâche</option>
+								<option value="rappel">Rappel</option>
+							</select>
+						</div>
+						<div class="col form-group">
+							<label for="task_status">Statut</label>
+							<select name="task_status" id="task_status" class="form-control">
+								<option value="Normal">Normal</option>
+								<option value="Priorité">Priorité</option>
+								<option value="Urgent">Urgent</option>
+							</select>
+						</div>
 					</div>
 
 					<div class="form-group">
-						<label for="due_date">Date due</label>
-						<input type="date" name="due_date" id="due_date" class="form-control">
+						<label for="task_title">Titre du tâche</label>
+						<input type="text" name="task_title" id="task_title" class="form-control" placeholder="Entrer le titre du tâche">
 					</div>
 
-					<div class="form-group">
-						<label for="task_client">Client</label>
-						<select name="task_client" id="task_client" class="form-control">
-							<option value="">Abbradebarras</option>
-							<option value="">AgoraJeux</option>
-						</select>
-					</div>
-
-					<div class="form-group">
-						<label for="task_am">AM</label>
-						<input type="text" readonly id="task_am" value="Utilisateur connecté: Mavreen Bassin" class="form-control">
-					</div>
-
-					<div class="form-group">
-						<label for="task_tm">TM</label>
-						<select name="task_tm" id="task_tm" class="form-control">
-							<option value="">Admin Adsrank</option>
-							<option value="">Dev Miora</option>
-						</select>
-					</div>
-
-					<div class="form-group">
-						<label for="levier_marketing">Leviers marketing</label>
-						<input type="text" value="Google Ads" id="levier_marketing" name="levier_marketing" class="form-control">
+					<div class="form-row">
+						<div class="col form-group">
+							<label for="add_member">Add Members</label>
+							<button type="button" class="btn btn-outline-dark rounded-circle d-block">
+								<i class="fa fa-user-plus"></i>
+							</button>
+						</div>
+						<div class="col form-group">
+							<label for="add_member">Add Labels</label>
+							<button type="button" class="btn btn-outline-dark rounded-circle d-block">
+								<i class="fa fa-tag"></i>
+							</button>
+						</div>
+						<div class="col form-group">
+							<label for="due_date">Date due</label>
+							<input type="date" name="due_date" id="due_date" class="form-control">
+						</div>
 					</div>
 
 					<div class="form-group">
@@ -53,12 +60,24 @@
 					</div>
 
 					<div class="form-group">
+						<label for="attachment">Attachment</label>
+						<div class="file-drop-area" id="fileDrop">
+							<div class="file-drop-icon">
+								<i class="fas fa-image"></i>
+							</div>
+							<span>Drag files here or <span class="text-primary">Browse</span></span>
+						</div>
+						<input type="file" id="fileInput" class="d-none">
+						<div id="fileName" class="mt-3 text-muted"></div>
+					</div>
+
+					<!-- <div class="form-group">
 						<label for="task_status">Statut</label>
 						<select name="task_status" id="task_status" class="form-control">
 							<option value="">Plannifier</option>
 							<option value="">En Cours</option>
 						</select>
-					</div>
+					</div> -->
 				</div>
 				<div class="modal-footer">
 					<button type="submit" class="btn btn-dark px-3">Ajouter</button>
