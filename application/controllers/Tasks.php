c31c77f99ba577f30f7ddb7b9cd515260fa6f352
Miora <razakatiana.antenaina@yahoo.com>
2025-08-17 11:36:27 +0200

    task

--- application/controllers/Tasks.php
+++ application/controllers/Tasks.php
@@ -18,8 +18,38 @@ class Tasks extends MY_Controller
     public function index()
     {
         $data['tasks'] = $this->Task_model->get_all_tasks();
+        $data['users'] = $this->Task_model->get_all_users();
         $this->load->view('layouts/tasks/list_tasks', $data);
     }
+    public function insert_tache() {
+		// Récupérer les données envoyées par le formulaire
+		$type_tache = $this->input->post('type_tache');
+		$date_demande = $this->input->post('date_demande');
+       
+		$date_due = $this->input->post('date_due');
+		$idclients = $this->input->post('idclients');
+		$AM = $this->input->post('AM');
+		$assigned_to = $this->input->post('assigned_to');
+		$title = $this->input->post('title');
+		$Statuts_technique = $this->input->post('Statuts_technique');
+		$tache = $this->input->post('tache');
+		$reference =
+			$data = array(
+				'type_tache' => $type_tache,
+				'date_demande' => $date_demande,
+				'date_due' => $date_due,
+				'idclients' => $idclients,
+				'AM' => $AM,
+				'assigned_to' => $assigned_to,
+				'title' => $title,
+				'Statuts_technique' => $Statuts_technique,
+				'description' => $tache
+			);
+
+		$this->Task_model->add_task($data);
+		$this->session->set_flashdata('message-succes', "Tâche ajoutée avec succès");
+		redirect('Task', 'refresh');
+	}
 
     public function view($task_id)
     {
