eddb2b05bca271c961f2a684419f8bb7eb7563ed
RANDRIANJAFINJOHARY Fandresena Notahiana <fandresena.randrianjafinjohary@cpp-system.com>
2025-08-19 11:08:40 +0300

    Commented Note | resolved task | sidebar fixe

--- application/controllers/Notes.php
+++ application/controllers/Notes.php
@@ -1,66 +1,63 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Notes extends MY_Controller {
+class Notes extends MY_Controller
+{
 
-    private $current_user;
+	private $current_user;
 
-    public function __construct() {
-        parent::__construct();
-        $this->load->model('Note_model');
-        $this->load->library('ion_auth');
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('Note_model');
+		$this->load->library('ion_auth');
 
-        // Utilisateur connecté
-        $this->current_user = $this->ion_auth->user()->row();
+		// Utilisateur connecté
+		$this->current_user = $this->ion_auth->user()->row();
+	}
 
-        if (!$this->ion_auth->logged_in()) {
-            redirect('auth/login');
-        }
-    }
+	public function index()
+	{
 
-    public function index() {
-
-        $this->data['notes'] = $this->Note_model->get_for_user($this->current_user->id);
-		
-    	$this->data['donnee'] = $this->visuels_model->getClientDataByDonnee();	
+		$notes = $this->data['notes'] = $this->Note_model->get_for_user($this->current_user->id);
+		$this->data['donnee'] = $this->visuels_model->getClientDataByDonnee();
 		$this->data['users'] = $this->visuels_model->getusersall();
+
 		// dd($this->data['notes']);
 		$this->content = "layouts/note/index.php";
 		$this->layout();
 
-        // $this->load->view('layouts/note/list', $data);
-    }
+		// $this->load->view('layouts/note/list', $data);
+	}
 
-    public function create() {
-    if ($this->input->method() === 'post') {
-        $noteData = [
-            'title'       => $this->input->post('title', TRUE),
-            'content'     => $this->input->post('content', TRUE),
-            'type'        => $this->input->post('type', TRUE),
-            'status'      => $this->input->post('status', TRUE),
-            'created_by'  => $this->current_user->id
-        ];
+	public function create()
+	{
+		if ($this->input->method() === 'post') {
+			$noteData = [
+				'title'       => $this->input->post('title', TRUE),
+				'content'     => $this->input->post('content', TRUE),
+				'type'        => $this->input->post('type', TRUE),
+				'status'      => $this->input->post('status', TRUE),
+				'created_by'  => $this->current_user->id
+			];
 
-        $assignedUsers = [];
+			$assignedUsers = [];
 
-        if ($this->input->post('assign_mode') === 'self') {
-            $assignedUsers[] = $this->current_user->id;
-        } else {
-            $assignedUsers = $this->input->post('assigned_to') ?? [];
-        }
+			if ($this->input->post('assign_mode') === 'self') {
+				$assignedUsers[] = $this->current_user->id;
+			} else {
+				$assignedUsers = $this->input->post('assigned_to') ?? [];
+			}
 
-        if (empty($assignedUsers)) {
-            $assignedUsers[] = $this->current_user->id; // sécurité
-        }
+			if (empty($assignedUsers)) {
+				$assignedUsers[] = $this->current_user->id; // sécurité
+			}
 
-        $this->Note_model->create($noteData, $assignedUsers);
-        redirect('notes');
-    }
+			$this->Note_model->create($noteData, $assignedUsers);
+			redirect('notes');
+		}
 
-    $data['users'] = $this->Note_model->get_all_users();
-    $this->load->view('layouts/note/create', $data);
-}
-
-
-
+		$data['users'] = $this->Note_model->get_all_users();
+		$this->load->view('layouts/note/create', $data);
+	}
 }
