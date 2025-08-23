a811be81ff7a711987684c224517047c641e9b70
Miora <razakatiana.antenaina@yahoo.com>
2025-08-23 15:51:47 +0200

    Ao
    Former-commit-id: 4b0e7134b9fc57b44d615e518217c0a735c44643

---
+++ application/views/layouts/plan_de_taggage/modal/discussion.php
@@ -0,0 +1,23 @@
+<div class="modal fade" id="discussionModal" tabindex="-1" aria-labelledby="discussionModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-dialog-scrollable">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="discussionModalLabel">Discussion</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+
+			<div class="modal-body" id="task_discussion"></div>
+
+			<div class="modal-footer">
+				<div class="input-group">
+					<textarea name="message" id="message" class="form-control p-2" style="resize: none;" placeholder="Type a message ..."></textarea>
+					<div class="input-group-append">
+						<button class="btn btn-primary" type="button">Envoyer</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
