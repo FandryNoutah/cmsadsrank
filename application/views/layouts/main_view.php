56692c83e631d4733de0c02d14adfb9e830fd05e
RANDRIANJAFINJOHARY Fandresena Notahiana <fandresena.randrianjafinjohary@cpp-system.com>
2025-08-25 22:44:39 +0300

    Finished all appart filter and sort by

--- application/views/layouts/main_view.php
+++ application/views/layouts/main_view.php
@@ -8,7 +8,6 @@
 	<link rel="stylesheet" href="<?= base_url('assets/vendors/fontawesome/css/all.min.css') ?>" />
 
 	<style>
-
 		#sidebarMenu.collapsed {
 			width: 75px !important;
 			transition: width 0.3s ease;
@@ -26,19 +25,34 @@
 			transition: width 0.3s ease;
 		}
 
-		.logo-full, .logo-collapsed {
+		.logo-full,
+		.logo-collapsed {
 			transition: opacity 0.2s ease;
 		}
 
+		#toggleSidebar {
+			left: 235px;
+			top: 21px;
+			z-index: 99999;
+			transition: left 0.3s ease;
+		}
+
+		#toggleSidebar.toggled {
+			left: 61px;
+			transition: left 0.3s ease;
+		}
 	</style>
-	
+
 	<?= $stylesheet ?>
 
 </head>
 
 <body>
 
-	<div class="container-fluid p-0 h-100">
+	<div class="container-fluid p-0 h-100 position-relative">
+		<button class="btn btn-light py-0 px-1 position-absolute border" id="toggleSidebar">
+			<img class="img" src="<?= base_url('assets/images/icons/figma/Menu/CaretDoubleHorizontal.png') ?>">
+		</button>
 		<div class="row no-gutters h-100">
 
 			<?= $sidebar ?>
@@ -74,7 +88,55 @@
 				$('.nav-label').toggleClass("d-none");
 
 				$('.logo-full').toggleClass('d-none');
-  				$('.logo-split').toggleClass('d-none');
+				$('.logo-split').toggleClass('d-none');
+
+				$(this).toggleClass('toggled');
+			});
+
+			$('#search_client').keyup(function(event) {
+
+				let keyword = $(this).val();
+				if (keyword) {
+
+					$(this).dropdown('show');
+
+					$.ajax({
+						type: "POST",
+						url: "<?= site_url('Client/search') ?>",
+						data: {
+							"keyword": keyword,
+						},
+						dataType: "json",
+						beforeSend: function() {
+							$('#clients_dropdown').html('<div class="w-100 dropdown-item text-center"><span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span></div>');
+						},
+						success: function(response) {
+
+							$('#clients_dropdown').html("");
+
+							if (response.length > 0) {
+
+								$.each(response, function(index, client) {
+
+									let id = client.idclients;
+									let name = client.nom_client;
+
+									let a = `
+										<a href="<?= site_url('Client/detail_client'); ?>/${id}" class="dropdown-item">
+											${name}
+										</a>
+									`;
+
+									$('#clients_dropdown').append(a);
+								});
+							} else {
+								$('#clients_dropdown').html('<div class="dropdown-item text-center">Aucun client trouvé!</div>');
+							}
+						}
+					});
+				} else {
+					$(this).dropdown('hide');
+				}
 			});
 		});
 	</script>
