d395d10e31603c7727b29ce34770ccca7c47753e
RANDRIANJAFINJOHARY Fandresena Notahiana <fandresena.randrianjafinjohary@cpp-system.com>
2025-08-28 11:45:05 +0300

    Finished filter

--- application/views/layouts/client/index.php
+++ application/views/layouts/client/index.php
@@ -92,34 +92,34 @@ Client
 <?php start_section('content'); ?>
 
 <div class="container-fluid">
-	
+
 	<div class="btn-group btn-group-toggle my-4" data-toggle="buttons">
 		<label class="btn btn-light rounded-pill mx-2" style="font-size: 14px;">
-			<input type="radio" name="options" id="option1" checked>
+			<input type="radio" class="status-select" name="status_filter" value="0" checked>
 			All Companies
 		</label>
 		<label class="btn btn-white rounded-pill mx-2 text-muted" style="font-size: 14px;">
-			<input type="radio" name="options" id="option2">
+			<input type="radio" class="status-select" name="status_filter" value="1">
 			<i class="fa fa-circle mr-2" style="font-size: 10px; color: #589E67;"></i>
 			Active
 		</label>
 		<label class="btn btn-white rounded-pill mx-2 text-muted" style="font-size: 14px;">
-			<input type="radio" name="options" id="option3">
+			<input type="radio" class="status-select" name="status_filter" value="2">
 			<i class="fa fa-circle mr-2" style="font-size: 10px; color: #B1AD1B;"></i>
 			Pause
 		</label>
 		<label class="btn btn-white rounded-pill mx-2 text-muted" style="font-size: 14px;">
-			<input type="radio" name="options" id="option3">
+			<input type="radio" class="status-select" name="status_filter" value="3">
 			<i class="fa fa-circle mr-2" style="font-size: 10px; color: #AF4B4B;"></i>
 			Résilier
 		</label>
 	</div>
 
 	<div class="tab-content" id="clientTabContent">
-	
+
 		<div class="tab-pane fade show active mb-5" id="list" role="tabpanel" aria-labelledby="list_tab">
 			<div class="table-responsive">
-	
+
 				<table class="table table-wrapper">
 					<thead class="bg-light text-muted">
 						<tr>
@@ -152,14 +152,14 @@ Client
 					<tbody>
 						<?php foreach ($donnee as $d): ?>
 							<?php if ($d->budget != 0) : ?>
-								<tr>
+								<tr class="client-filter" data-status="<?= $d->resiliation; ?>">
 									<td>
 										<a href="<?= base_url('Client/detail_client/' . $d->idclients) ?>" style="display: flex; align-items: center; text-decoration: none; color: inherit;">
 											<img src="<?= $d->favicon ?>" class="img-thumbnail" width="28" height="28" alt="Client Image" style="margin-right: 8px;">
 											<?= htmlspecialchars($d->nom_client) ?>
 										</a>
 									</td>
-	
+
 									<td class="text-muted"><?= $d->label_produit ?></td>
 									<td>
 										<img src="<?= base_url(IMAGES_PATH . htmlspecialchars($d->tech_photo_user)); ?>" width="28" height="28" alt="Client Image"><img src="<?= base_url(IMAGES_PATH . htmlspecialchars($d->am_photo_user)); ?>" width="28" height="28" alt="Client Image">
@@ -172,23 +172,23 @@ Client
 										?>
 										<?= $budget; ?> €</td>
 									<td>
-										<?php if($d->resiliation == 1):  ?>
-										<span class="badge alert-success rounded-pill px-2 py-1" style="font-size: 12px; font-weight: 500;">
-											<i class="fa fa-circle mr-1" style="font-size: 10px;"></i>
-											Active
-										</span>
+										<?php if ($d->resiliation == 1):  ?>
+											<span class="badge alert-success rounded-pill px-2 py-1" style="font-size: 12px; font-weight: 500;">
+												<i class="fa fa-circle mr-1" style="font-size: 10px;"></i>
+												Active
+											</span>
 										<?php endif; ?>
-										<?php if($d->resiliation == 2):  ?>
-										<span class="badge alert-warning rounded-pill px-2 py-1" style="font-size: 12px; font-weight: 500;">
-											<i class="fa fa-circle mr-1" style="font-size: 10px;"></i>
-											Mis en pause
-										</span>
+										<?php if ($d->resiliation == 2):  ?>
+											<span class="badge alert-warning rounded-pill px-2 py-1" style="font-size: 12px; font-weight: 500;">
+												<i class="fa fa-circle mr-1" style="font-size: 10px;"></i>
+												Mis en pause
+											</span>
 										<?php endif; ?>
-										<?php if($d->resiliation == 3):  ?>
-										<span class="badge alert-danger rounded-pill px-2 py-1" style="font-size: 12px; font-weight: 500;">
-											<i class="fa fa-circle mr-1" style="font-size: 10px;"></i>
-											Résilié
-										</span>
+										<?php if ($d->resiliation == 3):  ?>
+											<span class="badge alert-danger rounded-pill px-2 py-1" style="font-size: 12px; font-weight: 500;">
+												<i class="fa fa-circle mr-1" style="font-size: 10px;"></i>
+												Résilié
+											</span>
 										<?php endif; ?>
 									</td>
 								</tr>
@@ -198,10 +198,10 @@ Client
 				</table>
 			</div>
 		</div>
-	
+
 		<div class="tab-pane fade" id="kanban" role="tabpanel" aria-labelledby="kanban_tab">
 			<div class="row row-cols-3">
-	
+
 				<!-- ACTIVE COLUMN -->
 				<div class="col mb-3">
 					<div class="card h-100" style="border-radius: 8px;">
@@ -214,7 +214,7 @@ Client
 								<span class="badge alert-light mx-1 text-dark" style="font-size: 14px;">4</span>
 								<a href="#" class="text-dark text-decoration-none ml-auto" style="font-size: 28px;">+</a>
 							</div>
-	
+
 							<?php foreach ($donnee as $d): ?>
 								<?php if ($d->budget != 0) : ?>
 									<div class="card mt-3">
@@ -247,7 +247,7 @@ Client
 						</div>
 					</div>
 				</div>
-	
+
 				<!-- PAUSE COLUMN -->
 				<div class="col mb-3">
 					<div class="card h-100" style="border-radius: 8px;">
@@ -260,7 +260,7 @@ Client
 								<span class="badge alert-light mx-1 text-dark" style="font-size: 14px;">4</span>
 								<a href="#" class="text-dark text-decoration-none ml-auto" style="font-size: 28px;">+</a>
 							</div>
-	
+
 							<div class="card mt-3">
 								<div class="card-body">
 									<div class="d-block mb-3">
@@ -309,7 +309,7 @@ Client
 						</div>
 					</div>
 				</div>
-	
+
 				<!-- RESILIER COLUMN -->
 				<div class="col mb-3">
 					<div class="card h-100" style="border-radius: 8px;">
@@ -322,7 +322,7 @@ Client
 								<span class="badge alert-light mx-1 text-dark" style="font-size: 14px;">4</span>
 								<a href="#" class="text-dark text-decoration-none ml-auto" style="font-size: 28px;">+</a>
 							</div>
-	
+
 							<div class="card mt-3">
 								<div class="card-body">
 									<div class="d-block mb-3">
@@ -348,4 +348,31 @@ Client
 
 <?php $this->load->view('layouts/client/modal.php'); ?>
 
+<?php end_section(); ?>
+
+<?php start_section('script'); ?>
+
+<script>
+	$(function() {
+		$('.status-select').change(function() {
+
+			let status = $(this).val();
+
+			let $labels = $('.status-select').parent('label');
+
+			$labels.removeClass('btn-light').addClass('btn-white text-muted');
+			$labels.find(`.status-select[value="${status}"]`)
+				.parent('label')
+				.removeClass('btn-white text-muted')
+				.addClass('btn-light');
+
+			if (status == 0) {
+				$('.client-filter').removeClass('d-none');
+			} else {
+				$('.client-filter').addClass('d-none');
+				$('.client-filter[data-status="' + status + '"]').removeClass('d-none');
+			}
+		});
+	});
+</script>
 <?php end_section(); ?>
