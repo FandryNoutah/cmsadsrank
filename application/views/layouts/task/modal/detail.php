31271911d257dbe0a167c3eab6324d3820a6a705
RANDRIANJAFINJOHARY Fandresena Notahiana <fandresena.randrianjafinjohary@cpp-system.com>
2025-08-31 22:11:34 +0300

    Basic correction

--- application/views/layouts/task/modal/detail.php
+++ application/views/layouts/task/modal/detail.php
@@ -13,19 +13,13 @@
 				<div class="row">
 					<div class="col form-group">
 						<label for="add_member">Members</label>
-						<div class="d-flex align-items-center avatar-group">
-							
-						</div>
+						<div class="d-flex align-items-center avatar-group" id="detail_avatar"></div>
 					</div>
 					<div class="col form-group">
 						<label for="add_member">Label</label>
 						<div class="d-flex align-items-center">
-							<div class="mr-2">
-								<span class="badge alert-success p-2" style="font-size: 14px;">Internal</span>
-							</div>
-							<div class="mr-2">
-								<span class="badge alert-warning p-2" style="font-size: 14px;">Marketing</span>
-							</div>
+							<div class="mr-2" id="detail_type"></div>
+							<div class="mr-2" id="detail_status"></div>
 						</div>
 					</div>
 					<div class="col form-group">
@@ -35,18 +29,19 @@
 				</div>
 
 				<label>Description</label>
-				<p class="text-muted" id="detail_description">Monthly Product Discussion by Design and Marketing Teams with CEO to Plan our future products sales and reports</p>
+				<p class="text-muted" id="detail_description"></p>
 
 				<div class="form-group">
-					<label for="attachment">Attachment</label>
-					<div class="file-drop-area" id="fileDrop">
-						<div class="file-drop-icon">
-							<i class="fas fa-image"></i>
-						</div>
-						<span>Drag files here or <span class="text-primary">Browse</span></span>
+					<p>Attachment</p>
+					<div class="btn-group" role="group">
+						<button class="btn btn-outline-dark btn-sm" type="button">
+							<i class="fa fa-image"></i>
+						</button>
+						<button class="btn btn-outline-primary btn-sm">
+							<i class="fa fa-download"></i>
+							Télécharger le fichier
+						</button>
 					</div>
-					<input type="file" id="fileInput" class="d-none">
-					<div id="fileName" class="mt-3 text-muted"></div>
 				</div>
 
 				<form action="<?= site_url('Task/send_message') ?>" method="POST" id="detail_discussion_form">
