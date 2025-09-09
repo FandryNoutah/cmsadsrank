cf1b64409758ab57ee75fb0b2071c614066aeb58
Miora <razakatiana.antenaina@yahoo.com>
2025-09-09 11:56:10 +0200

    Discussion

--- application/models/Dashboard_model.php
+++ application/models/Dashboard_model.php
@@ -7,7 +7,40 @@ class Dashboard_model extends CI_Model {
 	protected $table3 = "clients";
 	protected $table4 = "donnee";
 	protected $table5 = "upsell";
+	public function get_all_tâche($status = null)
+	{
+		$this->db->select('tasks.*, u1.first_name as assigned_to_name, u1.photo_users as assigned_to_photo, u2.first_name as AM_name, u2.photo_users as AM_photo, clients.*');
+		$this->db->from('tasks');
 
+		// Jointure avec la table users pour "assigned_to" (utilisateur assigné à la tâche)
+		$this->db->join('users u1', 'u1.id = tasks.assigned_to', 'left');
+
+		// Jointure avec la table clients
+		$this->db->join('clients', 'clients.idclients = tasks.idclients', 'left');
+
+		// Jointure avec la table users pour "AM" (utilisateur AM)
+		$this->db->join('users u2', 'u2.id = tasks.AM', 'left');
+
+		if ($status !== null) {
+			$this->db->where('Statuts_technique', $status); // Exact match
+			// OR use $this->db->where('Statuts_technique !=', $status) if you want "not equal"
+		}
+
+		// Exécution de la requête
+		$query = $this->db->get();
+
+		// Retourner les résultats
+		return $query->result();
+	}
+	public function get_for_user($user_id) {
+        $this->db->select('n.*, u.username AS author');
+        $this->db->from('notes n');
+        $this->db->join('users u', 'u.id = n.created_by', 'left');
+        $this->db->join('note_users nu', 'nu.note_id = n.id', 'inner');
+        $this->db->where('nu.user_id', $user_id);
+        $this->db->order_by('n.created_at', 'DESC');
+        return $this->db->get()->result();
+    }	
 	public function get_discussion_task_by_id_users($idusers)
 	{
 		$sql = "SELECT group_messages.*, users.*
