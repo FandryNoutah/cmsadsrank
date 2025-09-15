57b96dbe36bc6270971ef992470ec1d8c0c47e7e
RANDRIANJAFINJOHARY Fandresena Notahiana <fandresena.randrianjafinjohary@cpp-system.com>
2025-09-15 23:03:32 +0300

    Finalized basic discussion, missing some backend

--- application/controllers/Discussion.php
+++ application/controllers/Discussion.php
@@ -1,44 +1,40 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
- 
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
 class Discussion extends MY_Controller
 {
 	protected $data;
- 
-	function __construct() {
+
+	function __construct()
+	{
 		parent::__construct();
 		$this->load->model("Discussion_model");
 		$this->load->model("Note_model");
 		$this->load->model("Task_message_model");
+		$this->load->model('Note_message_model');
 
 		$this->current_user = $this->ion_auth->user()->row();
 		$idusers = $this->current_user->id;
-		
-		$this->data['nbr_discussion_gtm'] = count($this->Discussion_model->get_discussion_gtm_by_id_users($idusers));
+
+		$count_gtm = $this->data['nbr_discussion_gtm'] = count($this->Discussion_model->get_discussion_gtm_by_id_users($idusers));
+		$count_task = $this->data['nbr_discussion_task'] = count($this->Discussion_model->get_discussion_task_by_id_users($idusers));
+		$count_note = $this->data['nbr_discussion_note'] = count($this->Discussion_model->get_discussion_note_by_id_users($idusers));
+		$this->data['nbr_discussion_all'] = $count_gtm + $count_task + $count_note;
 	}
 
 	public function index()
 	{
 		$idusers = $this->current_user->id;
-		$discussion = $this->data['discussion_note'] = $this->Discussion_model->get_discussion_gtm_by_id_users($idusers);
-		$notes = $this->Note_model->get_for_user($this->current_user->id);
-		$this->data['users'] = $this->Note_model->get_all_users();
-
-		foreach ($notes as $note) {
-
-			$id_note = $note->id;
-			$assigned_users = $this->Note_model->get_assigned_users($id_note);
-			$note->assigned_users = $assigned_users;
-		}
-
-		$this->data['notes'] = $notes;
+		// $this->data['gtm'] = $this->Discussion_model->get_discussion_gtm_by_id_users($idusers);
+		$this->data['notes'] = $this->Note_model->get_for_user($idusers);;
 		$this->data['taches'] = $this->Task_model->get_all_tâche();
+
 		$this->content = "layouts/discussion/index.php";
 		$this->layout();
 	}
 
 	public function Team_task()
 	{
-		
+
 		$idusers = $this->current_user->id;
 		$discussion = $this->data['discussion_note'] = $this->Discussion_model->get_discussion_gtm_by_id_users($idusers);
 		$notes = $this->Note_model->get_for_user($this->current_user->id);
@@ -63,7 +59,7 @@ class Discussion extends MY_Controller
 				case "planifié":
 					$this->data['count_planned']++;
 					break;
-				
+
 				case "en cours":
 					$this->data['count_upcoming']++;
 					break;
@@ -80,20 +76,9 @@ class Discussion extends MY_Controller
 	public function Brief()
 	{
 		$idusers = $this->current_user->id;
-		$discussion = $this->data['discussion_note'] = $this->Discussion_model->get_discussion_gtm_by_id_users($idusers);
-		$this->data['nbr_discussion_gtm'] = $nbr_discussion_gtm = count($discussion);
-		$notes = $this->Note_model->get_for_user($this->current_user->id);
-		$this->data['users'] = $this->Note_model->get_all_users();
+		// Change to brief
+		$this->data['notes'] = $this->Note_model->get_for_user($idusers);
 
-		foreach ($notes as $note) {
-
-			$id_note = $note->id;
-			$assigned_users = $this->Note_model->get_assigned_users($id_note);
-			$note->assigned_users = $assigned_users;
-		}
-
-		$this->data['notes'] = $notes;
-		$this->data['tache'] = $this->Task_model->get_all_tâche();
 		$this->content = "layouts/discussion/brief/index.php";
 		$this->layout();
 	}
@@ -101,28 +86,20 @@ class Discussion extends MY_Controller
 	public function Temporaire()
 	{
 		$idusers = $this->current_user->id;
-		$discussion = $this->data['discussion_note'] = $this->Discussion_model->get_discussion_gtm_by_id_users($idusers);
-		$this->data['nbr_discussion_gtm'] = $nbr_discussion_gtm = count($discussion);
-		$notes = $this->Note_model->get_for_user($this->current_user->id);
-		$this->data['users'] = $this->Note_model->get_all_users();
+		// Change to temporaire
+		$this->data['notes'] = $this->Note_model->get_for_user($idusers);
 
-		foreach ($notes as $note) {
-
-			$id_note = $note->id;
-			$assigned_users = $this->Note_model->get_assigned_users($id_note);
-			$note->assigned_users = $assigned_users;
-		}
-
-		$this->data['notes'] = $notes;
-		$this->data['tache'] = $this->Task_model->get_all_tâche();
 		$this->content = "layouts/discussion/temporaire/index.php";
 		$this->layout();
 	}
 
 	public function Gtm()
 	{
-		$tasks = $this->data['taches'] = $this->Task_model->get_all_tâche();
-		$this->data['count_planned'] = 0;
+		$idusers = $this->current_user->id;
+		// Change to gtm
+		$this->data['notes'] = $this->Note_model->get_for_user($idusers);
+
+		/* $this->data['count_planned'] = 0;
 		$this->data['count_upcoming'] = 0;
 		$this->data['count_completed'] = 0;
 
@@ -132,7 +109,7 @@ class Discussion extends MY_Controller
 				case "planifié":
 					$this->data['count_planned']++;
 					break;
-				
+
 				case "en cours":
 					$this->data['count_upcoming']++;
 					break;
@@ -141,25 +118,46 @@ class Discussion extends MY_Controller
 					$this->data['count_completed']++;
 					break;
 			}
-		}
+		} */
 		$this->content = "layouts/discussion/gtm/index.php";
 		$this->layout();
 	}
 
-	public function fetch_discussion($id_task)
+	public function fetch_discussion()
 	{
 
-		// Check for order (ascendant || descendant)
-
-		$messages = $this->Task_message_model->get_messages_by_task($id_task);
 		$currentUser = $this->current_user;
+		$id = $this->input->post('id', TRUE);
+		$type = $this->input->post('type', TRUE);
+
+		switch ($type) {
+			case 'task':
+				$messages = $this->Task_message_model->get_messages_by_task($id);
+				break;
+
+			case 'note':
+				$messages = $this->Note_message_model->get_messages_by_note($id);
+				break;
+
+			case 'gtm':
+				$messages = $this->Discussion_model->get_discussion_gtm_by_id_users($id);
+				break;
+
+			case 'temporaire':
+				// get temporaire messages
+				break;
+
+			case 'team_task':
+				// get team_task messages
+				break;
+		}
 
 		foreach ($messages as $message) {
 
 			$created_at = $message->created_at;
 			$message->created_at = (new DateTime($created_at))->format('j M, H:i');
 
-			$message->owner = $message->user_id == $currentUser->id;
+			$message->owner = $message->user_id === $currentUser->id;
 		}
 
 		echo json_encode($messages);
@@ -168,20 +166,45 @@ class Discussion extends MY_Controller
 	public function send_message()
 	{
 
-		$id_task = $this->input->post('id_task', TRUE);
+		$id = $this->input->post('id', TRUE);
+		$type = $this->input->post('type', TRUE);
 		$message = $this->input->post('message', TRUE);
 
 		if (!empty($message) && $this->current_user) {
-			$this->Task_message_model->insert_message([
-				'user_id' => $this->current_user->id,
-				'task_id' => $id_task,
-				'message' => $message
-			]);
+			
+			switch ($type) {
+				case 'task':
+					$this->Task_message_model->insert_message([
+						'user_id' => $this->current_user->id,
+						'task_id' => $id,
+						'message' => $message
+					]);
+					break;
+
+				case 'note':
+					$this->Note_message_model->insert_message([
+						'user_id' => $this->current_user->id,
+						'id_notes' => $id,
+						'message' => $message
+					]);
+					break;
+
+				case 'gtm':
+					// insert message gtm
+					break;
+
+				case 'temporaire':
+					// insert message temporaire
+					break;
+
+				case 'team_task':
+					// insert message task
+					break;
+			}
 		}
 
 		echo json_encode([
 			"done"	=>	true
 		]);
 	}
-
 }
