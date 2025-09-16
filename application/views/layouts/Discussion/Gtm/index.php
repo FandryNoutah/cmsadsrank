a4b80630039fd109ed9c6323b6fa95d735634d7f
RANDRIANJAFINJOHARY Fandresena Notahiana <fandresena.randrianjafinjohary@cpp-system.com>
2025-09-16 22:02:58 +0300

    Finished messages count

--- application/views/layouts/Discussion/Gtm/index.php
+++ application/views/layouts/Discussion/Gtm/index.php
@@ -12,21 +12,26 @@ Discussion
 		<div class="container-fluid">
 			<h5 class="mb-4">📥 Message Brief <small>(<?= count($tasks); ?>)</small></h5>
 			<?php foreach ($tasks as $task): ?>
-				<div class="card mb-3">
-					<div class="card-body">
-						<div class="d-flex justify-content-between mb-2">
-							<span class="font-weight-bold"><?= nl2br(htmlspecialchars($taREMOVED>username)); ?></span>
-							<span class="text-muted small"><?= nl2br(htmlspecialchars($taREMOVED>date_due)); ?></span>
+				<?php if ($taREMOVED>count_messages > 0): ?>
+					<div class="card mb-3">
+						<div class="card-body">
+							<div class="d-flex justify-content-between mb-2">
+								<span class="font-weight-bold"><?= nl2br(htmlspecialchars($taREMOVED>username)); ?></span>
+								<span class="text-muted small"><?= nl2br(htmlspecialchars($taREMOVED>date_due)); ?></span>
+							</div>
+
+							<div class="font-weight-bold mb-1"><?= htmlspecialchars($taREMOVED>title); ?></div>
+							<div class="text-muted small mb-2"><?= nl2br(htmlspecialchars($taREMOVED>description)); ?></div>
+
+							<button class="btn btn-light btn-sm mt-1">👍 4</button>
+							<button class="btn btn-light btn-sm mt-1" data-toggle="modal" data-target="#discussionModal" data-id="<?= $taREMOVED>idtask; ?>">
+								💬
+								<?= $taREMOVED>count_messages; ?>
+							</button>
+
 						</div>
-
-						<div class="font-weight-bold mb-1"><?= htmlspecialchars($taREMOVED>title); ?></div>
-						<div class="text-muted small mb-2"><?= nl2br(htmlspecialchars($taREMOVED>description)); ?></div>
-
-						<button class="btn btn-light btn-sm mt-1">👍 4</button>
-						<button class="btn btn-light btn-sm mt-1" data-toggle="modal" data-target="#discussionModal" data-id="<?= $taREMOVED>idtask; ?>">💬 2</button>
-
 					</div>
-				</div>
+				<?php endif; ?>
 			<?php endforeach; ?>
 
 		</div>
