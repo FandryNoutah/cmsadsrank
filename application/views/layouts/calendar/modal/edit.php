3e922e9b4721f658ef9946a23c7274c563ee2daa
RANDRIANJAFINJOHARY Fandresena Notahiana <fandresena.randrianjafinjohary@cpp-system.com>
2025-09-18 13:40:52 +0300

    Basic google agenda styl

--- application/views/layouts/calendar/modal/edit.php
+++ application/views/layouts/calendar/modal/edit.php
@@ -1,6 +1,6 @@
 <form id="editEventForm">
-	<div class="modal fade" id="editEventModal" tabindex="-1" aria-labelledby="editEventModalLabel" aria-hidden="true">
-		<div class="modal-dialog modal-lg modal-dialog-scrollable" style="width: 640px;">
+	<div class="modal fade" id="editEventModal" aria-labelledby="editEventModalLabel" aria-hidden="true">
+		<div class="modal-dialog modal-dialog-scrollable">
 			<div class="modal-content">
 				<div class="modal-header">
 					<h5 class="modal-title" id="editEventModalLabel">Modifier un événement</h5>
@@ -13,30 +13,30 @@
 
 					<div class="form-group">
 						<label>Titre</label><br>
-						<input type="text" class="form-control" name="title"><br><br>
+						<input type="text" class="form-control" name="title">
 					</div>
 
 					<div class="form-group">
 						<label>Description</label><br>
-						<textarea name="description" class="form-control"></textarea><br><br>
+						<textarea name="description" class="form-control"></textarea>
 					</div>
 
 					<div class="form-group">
 						<label>Début</label><br>
-						<input type="datetime-local" class="form-control" name="start_date"><br><br>
+						<input type="datetime-local" class="form-control" name="start_date">
 					</div>
 
 					<div class="form-group">
 						<label>Fin</label><br>
-						<input type="datetime-local" class="form-control" name="end_date"><br><br>
+						<input type="datetime-local" class="form-control" name="end_date">
 					</div>
 
 					<label>Couleur</label><br>
-					<input type="color" name="color" value="#3788d8"><br><br>
+					<input type="color" name="color" value="#3788d8">
 
 					<div class="form-group">
 						<label>Participants</label><br>
-						<select name="attendees[]" id="editAttendeesSelect" class="form-control" multiple></select><br><br>
+						<select name="attendees[]" id="editAttendeesSelect" class="form-control" multiple></select>
 					</div>
 
 				</div>
