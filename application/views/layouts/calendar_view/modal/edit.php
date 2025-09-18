8b6e0690c1208d2776ce181616ecd7221b2b31ce
RANDRIANJAFINJOHARY Fandresena Notahiana <fandresena.randrianjafinjohary@cpp-system.com>
2025-09-18 08:01:02 +0300

    En cours calendar

---
+++ application/views/layouts/calendar_view/modal/edit.php
@@ -0,0 +1,13 @@
+<div class="modal fade" id="detailModal" tabindex="-1" aria-labelledby="detailModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-lg modal-dialog-scrollable" style="width: 640px;">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="detailModalLabel">Ajouter un événement</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body"></div>
+		</div>
+	</div>
+</div>
