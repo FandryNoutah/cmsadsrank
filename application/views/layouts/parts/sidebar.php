13fdb20c6dc45f0a02946ed99494347f7023ea2e
Miora <razakatiana.antenaina@yahoo.com>
2025-09-30 10:01:18 +0200

    Task modif
    Former-commit-id: 5970cd98469e377611c89c9b9d5bb09dee71c471

--- application/views/layouts/parts/sidebar.php
+++ application/views/layouts/parts/sidebar.php
@@ -48,6 +48,12 @@
 					<span class="nav-label">GTM</span>
 				</a>
 			</li>
+			<li class="nav-item rounded">
+				<a class="nav-link text-secondary" href="<?= base_url('Upsell') ?>">
+					<img class="mr-2" src="<?= base_url('assets/images/icons/figma/icon-gear.svg') ?>" />
+					<span class="nav-label">Upsell / Baisse</span>
+				</a>
+			</li>
 			
 		</ul>
 
