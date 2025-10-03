1da5a880e9ecb2d4260e77a3745db62c41e8d7cd
RANDRIANJAFINJOHARY Fandresena Notahiana <fandresena.randrianjafinjohary@cpp-system.com>
2025-10-03 10:26:08 +0300

    Working on campagne intersection
    Former-commit-id: 63684d101e2104ef9931622c1fd3c80e707a343c

--- application/views/layouts/client/onboarding/pmax.php
+++ application/views/layouts/client/onboarding/pmax.php
@@ -53,230 +53,231 @@
 			</nav>
 
 			<div class="col">
+				<form action='<?= site_url('Client/ajout_campagne/' . $idclients) . "?conversion=$conversion&camp_type=$camp_type&gtm=$gtm" ?>' method="POST">
+					<div class="container-fluid pt-4">
 
-				<div class="container-fluid pt-4">
+						<h5>Campagne Pmax</h5>
+						<hr class="my-4">
 
-					<h5>Campagne Pmax</h5>
-					<hr class="my-4">
-
-					<div class="row align-items-center mb-4">
-						<div class="col-auto">
-							<img src="<?= base_url('assets/images/figma/discu_queue.png') ?>" class="img-thumbnail rounded-circle" width="64">
-						</div>
-						<div class="col-auto">
-							<input type="hidden" name="file">
-							<button type="button" class="btn btn-light btn-sm">
-								<i class="fa fa-upload"></i>
-								Upload Company Logo
-							</button>
-						</div>
-					</div>
-
-					<div class="form-group">
-						<label for="camp_url">URL de la campagne</label>
-						<input type="text" class="form-control" name="camp_url">
-					</div>
-
-					<div class="form-group">
-						<label for="camp_budget">Budget de la campagne</label>
-						<input type="text" class="form-control" name="camp_budget">
-					</div>
-
-					<div class="form-group">
-						<label for="">Quels produits ou services promouvez-vous dans cette campagne ?</label>
-						<textarea name="" class="form-control" maxlength="50"></textarea>
-					</div>
-
-					<div class="form-group">
-						<label for="">En quoi vos produits ou services sont-ils uniques ?</label>
-						<textarea name="" class="form-control" maxlength="50"></textarea>
-					</div>
-
-					<h5>Paramètres de la campagne</h5>
-					<div class="form-group">
-						<label for="">Zone géographique</label>
-						<input type="text" class="form-control">
-					</div>
-
-					<div class="form-group">
-						<label for="">Langues</label>
-						<select name="" class="form-control">
-							<option value="">Lang 1</option>
-							<option value="">Lang 2</option>
-						</select>
-					</div>
-
-					<div class="form-group">
-						<label for="">Cibles</label>
-						<select name="" class="form-control">
-							<option value="">Cible 1</option>
-							<option value="">Cible 2</option>
-						</select>
-					</div>
-
-					<div class="form-group">
-						<label for="">Tranche d'âges</label>
-						<select name="" class="form-control">
-							<option value="">1</option>
-							<option value="">2</option>
-						</select>
-					</div>
-
-					<div class="form-group">
-						<label for="">Audiences</label>
-						<select name="" class="form-control">
-							<option value="">Audience 1</option>
-							<option value="">Audience 2</option>
-						</select>
-					</div>
-
-					<div class="container mb-3">
-						<div class="multi-col" style="height: 200px;">
-							<div class="custom-control custom-checkbox">
-								<input type="checkbox" name="" id="" class="custom-control-input">
-								<label for="" class="custom-control-label">Affinité</label>
+						<div class="row align-items-center mb-4">
+							<div class="col-auto">
+								<img src="<?= base_url('assets/images/figma/discu_queue.png') ?>" class="img-thumbnail rounded-circle" width="64">
 							</div>
-							<div class="custom-control custom-checkbox">
-								<input type="checkbox" name="" id="" class="custom-control-input">
-								<label for="" class="custom-control-label">Acheteurs</label>
-							</div>
-							<div class="custom-control custom-checkbox">
-								<input type="checkbox" name="" id="" class="custom-control-input">
-								<label for="" class="custom-control-label">Actualités et politique</label>
-							</div>
-							<div class="custom-control custom-checkbox">
-								<input type="checkbox" name="" id="" class="custom-control-input">
-								<label for="" class="custom-control-label">Alimentation et restauration</label>
-							</div>
-							<div class="custom-control custom-checkbox">
-								<input type="checkbox" name="" id="" class="custom-control-input">
-								<label for="" class="custom-control-label">Banque et finance</label>
-							</div>
-							<div class="custom-control custom-checkbox">
-								<input type="checkbox" name="" id="" class="custom-control-input">
-								<label for="" class="custom-control-label">Beauté et bien-être</label>
-							</div>
-							<div class="custom-control custom-checkbox">
-								<input type="checkbox" name="" id="" class="custom-control-input">
-								<label for="" class="custom-control-label">Maison et jardinage</label>
-							</div>
-							<div class="custom-control custom-checkbox">
-								<input type="checkbox" name="" id="" class="custom-control-input">
-								<label for="" class="custom-control-label">Modes de vie et loisirs</label>
-							</div>
-							<div class="custom-control custom-checkbox">
-								<input type="checkbox" name="" id="" class="custom-control-input">
-								<label for="" class="custom-control-label">Médias et divertissement</label>
-							</div>
-							<div class="custom-control custom-checkbox">
-								<input type="checkbox" name="" id="" class="custom-control-input">
-								<label for="" class="custom-control-label">Sport et remise en forme</label>
-							</div>
-							<div class="custom-control custom-checkbox">
-								<input type="checkbox" name="" id="" class="custom-control-input">
-								<label for="" class="custom-control-label">Technologie</label>
-							</div>
-							<div class="custom-control custom-checkbox">
-								<input type="checkbox" name="" id="" class="custom-control-input">
-								<label for="" class="custom-control-label">Voyages</label>
-							</div>
-							<div class="custom-control custom-checkbox">
-								<input type="checkbox" name="" id="" class="custom-control-input">
-								<label for="" class="custom-control-label">Véhicules et transports</label>
+							<div class="col-auto">
+								<input type="hidden" name="file">
+								<button type="button" class="btn btn-light btn-sm">
+									<i class="fa fa-upload"></i>
+									Upload Company Logo
+								</button>
 							</div>
 						</div>
-					</div>
 
-					<div class="form-group">
-						<label for="">Appareil</label>
-						<select name="" id="" class="form-control">
-							<option value="">Appareil 1</option>
-							<option value="">Appareil 2</option>
-						</select>
-					</div>
-
-					<button type="button" class="btn btn-dark btn-sm btn-block mb-5">Suivant</button>
-
-					<ul class="nav nav-tabs mb-3">
-						<li class="nav-item">
-							<a class="nav-link py-3 active" type="button">
-								Propositions de mots-clés à exclure
-							</a>
-						</li>
-					</ul>
-
-					<div class="card bg-light">
-						<div class="card-body">
-							<div class="multi-col" style="height: 550px;">
-								<p class="mb-1">micro entreprise</p>
-								<p class="mb-1">auto entrepreneur</p>
-								<p class="mb-1">microentreprise</p>
-								<p class="mb-1">création entreprise</p>
-								<p class="mb-1">freelance</p>
-								<p class="mb-1">business plan</p>
-								<p class="mb-1">statut juridique</p>
-								<p class="mb-1">financement pôle emploi</p>
-								<p class="mb-1">prime création</p>
-								<p class="mb-1">auto-entrepreneur</p>
-								<p class="mb-1">boutique en ligne</p>
-								<p class="mb-1">dropshipping</p>
-								<p class="mb-1">vinted</p>
-								<p class="mb-1">définition</p>
-								<p class="mb-1">comment faire</p>
-								<p class="mb-1">pdf</p>
-								<p class="mb-1">gratuit</p>
-								<p class="mb-1">exemple</p>
-								<p class="mb-1">livre blanc</p>
-								<p class="mb-1">template</p>
-								<p class="mb-1">coursseed</p>
-								<p class="mb-1">pre seed</p>
-								<p class="mb-1">early stage</p>
-								<p class="mb-1">incubateur</p>
-								<p class="mb-1">accélérateur</p>
-								<p class="mb-1">business angel</p>
-								<p class="mb-1">levée de fonds seed</p>
-							</div>
+						<div class="form-group">
+							<label for="url_campagne_pmax">URL de la campagne</label>
+							<input type="text" class="form-control" name="url_campagne_pmax" id="url_campagne_pmax">
 						</div>
-					</div>
 
-					<ul class="nav nav-tabs mb-3">
-						<li class="nav-item">
-							<a class="nav-link py-3 active" type="button">
-								Proposition d'images
-							</a>
-						</li>
-					</ul>
+						<div class="form-group">
+							<label for="repartition_budget_pmax">Budget de la campagne</label>
+							<input type="text" class="form-control" name="repartition_budget_pmax" id="repartition_budget_pmax">
+						</div>
 
-					<div class="card mb-4">
-						<div class="card-body">
-							<div class="row no-gutters">
-								<div class="col-auto px-2 mb-3">
-									<img src="<?= base_url('assets/images/formats/betewq5osgluxdan7v5blfsgba.jpg') ?>" alt="" width="120">
+						<div class="form-group">
+							<label for="">Quels produits ou services promouvez-vous dans cette campagne ?</label>
+							<textarea name="" class="form-control" maxlength="50"></textarea>
+						</div>
+
+						<div class="form-group">
+							<label for="">En quoi vos produits ou services sont-ils uniques ?</label>
+							<textarea name="" class="form-control" maxlength="50"></textarea>
+						</div>
+
+						<h5>Paramètres de la campagne</h5>
+						<div class="form-group">
+							<label for="">Zone géographique</label>
+							<input type="text" class="form-control">
+						</div>
+
+						<div class="form-group">
+							<label for="">Langues</label>
+							<select name="" class="form-control">
+								<option value="">Lang 1</option>
+								<option value="">Lang 2</option>
+							</select>
+						</div>
+
+						<div class="form-group">
+							<label for="">Cibles</label>
+							<select name="" class="form-control">
+								<option value="">Cible 1</option>
+								<option value="">Cible 2</option>
+							</select>
+						</div>
+
+						<div class="form-group">
+							<label for="">Tranche d'âges</label>
+							<select name="" class="form-control">
+								<option value="">1</option>
+								<option value="">2</option>
+							</select>
+						</div>
+
+						<div class="form-group">
+							<label for="">Audiences</label>
+							<select name="" class="form-control">
+								<option value="">Audience 1</option>
+								<option value="">Audience 2</option>
+							</select>
+						</div>
+
+						<div class="container mb-3">
+							<div class="multi-col" style="height: 200px;">
+								<div class="custom-control custom-checkbox">
+									<input type="checkbox" name="" id="" class="custom-control-input">
+									<label for="" class="custom-control-label">Affinité</label>
 								</div>
-								<div class="col-auto px-2 mb-3">
-									<img src="<?= base_url('assets/images/formats/betewq5osgluxdan7v5blfsgba.jpg') ?>" alt="" width="120">
+								<div class="custom-control custom-checkbox">
+									<input type="checkbox" name="" id="" class="custom-control-input">
+									<label for="" class="custom-control-label">Acheteurs</label>
 								</div>
-								<div class="col-auto px-2 mb-3">
-									<img src="<?= base_url('assets/images/formats/betewq5osgluxdan7v5blfsgba.jpg') ?>" alt="" width="120">
+								<div class="custom-control custom-checkbox">
+									<input type="checkbox" name="" id="" class="custom-control-input">
+									<label for="" class="custom-control-label">Actualités et politique</label>
 								</div>
-								<div class="col-auto px-2 mb-3">
-									<img src="<?= base_url('assets/images/formats/betewq5osgluxdan7v5blfsgba.jpg') ?>" alt="" width="120">
+								<div class="custom-control custom-checkbox">
+									<input type="checkbox" name="" id="" class="custom-control-input">
+									<label for="" class="custom-control-label">Alimentation et restauration</label>
 								</div>
-								<div class="col-auto px-2 mb-3">
-									<img src="<?= base_url('assets/images/formats/betewq5osgluxdan7v5blfsgba.jpg') ?>" alt="" width="120">
+								<div class="custom-control custom-checkbox">
+									<input type="checkbox" name="" id="" class="custom-control-input">
+									<label for="" class="custom-control-label">Banque et finance</label>
 								</div>
-								<div class="col-auto px-2 mb-3">
-									<img src="<?= base_url('assets/images/formats/betewq5osgluxdan7v5blfsgba.jpg') ?>" alt="" width="120">
+								<div class="custom-control custom-checkbox">
+									<input type="checkbox" name="" id="" class="custom-control-input">
+									<label for="" class="custom-control-label">Beauté et bien-être</label>
+								</div>
+								<div class="custom-control custom-checkbox">
+									<input type="checkbox" name="" id="" class="custom-control-input">
+									<label for="" class="custom-control-label">Maison et jardinage</label>
+								</div>
+								<div class="custom-control custom-checkbox">
+									<input type="checkbox" name="" id="" class="custom-control-input">
+									<label for="" class="custom-control-label">Modes de vie et loisirs</label>
+								</div>
+								<div class="custom-control custom-checkbox">
+									<input type="checkbox" name="" id="" class="custom-control-input">
+									<label for="" class="custom-control-label">Médias et divertissement</label>
+								</div>
+								<div class="custom-control custom-checkbox">
+									<input type="checkbox" name="" id="" class="custom-control-input">
+									<label for="" class="custom-control-label">Sport et remise en forme</label>
+								</div>
+								<div class="custom-control custom-checkbox">
+									<input type="checkbox" name="" id="" class="custom-control-input">
+									<label for="" class="custom-control-label">Technologie</label>
+								</div>
+								<div class="custom-control custom-checkbox">
+									<input type="checkbox" name="" id="" class="custom-control-input">
+									<label for="" class="custom-control-label">Voyages</label>
+								</div>
+								<div class="custom-control custom-checkbox">
+									<input type="checkbox" name="" id="" class="custom-control-input">
+									<label for="" class="custom-control-label">Véhicules et transports</label>
 								</div>
 							</div>
 						</div>
-					</div>
 
-					<div class="d-flex justify-content-between mb-5">
-						<button class="btn btn-outline-dark">Ajouter une nouvelle campagne</button>
-						<button class="btn btn-dark">Terminer</button>
+						<div class="form-group">
+							<label for="">Appareil</label>
+							<select name="" id="" class="form-control">
+								<option value="">Appareil 1</option>
+								<option value="">Appareil 2</option>
+							</select>
+						</div>
+
+						<button type="button" class="btn btn-dark btn-sm btn-block mb-5">Suivant</button>
+
+						<ul class="nav nav-tabs mb-3">
+							<li class="nav-item">
+								<a class="nav-link py-3 active" type="button">
+									Propositions de mots-clés à exclure
+								</a>
+							</li>
+						</ul>
+
+						<div class="card bg-light">
+							<div class="card-body">
+								<div class="multi-col" style="height: 550px;">
+									<p class="mb-1">micro entreprise</p>
+									<p class="mb-1">auto entrepreneur</p>
+									<p class="mb-1">microentreprise</p>
+									<p class="mb-1">création entreprise</p>
+									<p class="mb-1">freelance</p>
+									<p class="mb-1">business plan</p>
+									<p class="mb-1">statut juridique</p>
+									<p class="mb-1">financement pôle emploi</p>
+									<p class="mb-1">prime création</p>
+									<p class="mb-1">auto-entrepreneur</p>
+									<p class="mb-1">boutique en ligne</p>
+									<p class="mb-1">dropshipping</p>
+									<p class="mb-1">vinted</p>
+									<p class="mb-1">définition</p>
+									<p class="mb-1">comment faire</p>
+									<p class="mb-1">pdf</p>
+									<p class="mb-1">gratuit</p>
+									<p class="mb-1">exemple</p>
+									<p class="mb-1">livre blanc</p>
+									<p class="mb-1">template</p>
+									<p class="mb-1">coursseed</p>
+									<p class="mb-1">pre seed</p>
+									<p class="mb-1">early stage</p>
+									<p class="mb-1">incubateur</p>
+									<p class="mb-1">accélérateur</p>
+									<p class="mb-1">business angel</p>
+									<p class="mb-1">levée de fonds seed</p>
+								</div>
+							</div>
+						</div>
+
+						<ul class="nav nav-tabs mb-3">
+							<li class="nav-item">
+								<a class="nav-link py-3 active" type="button">
+									Proposition d'images
+								</a>
+							</li>
+						</ul>
+
+						<div class="card mb-4">
+							<div class="card-body">
+								<div class="row no-gutters">
+									<div class="col-auto px-2 mb-3">
+										<img src="<?= base_url('assets/images/formats/betewq5osgluxdan7v5blfsgba.jpg') ?>" alt="" width="120">
+									</div>
+									<div class="col-auto px-2 mb-3">
+										<img src="<?= base_url('assets/images/formats/betewq5osgluxdan7v5blfsgba.jpg') ?>" alt="" width="120">
+									</div>
+									<div class="col-auto px-2 mb-3">
+										<img src="<?= base_url('assets/images/formats/betewq5osgluxdan7v5blfsgba.jpg') ?>" alt="" width="120">
+									</div>
+									<div class="col-auto px-2 mb-3">
+										<img src="<?= base_url('assets/images/formats/betewq5osgluxdan7v5blfsgba.jpg') ?>" alt="" width="120">
+									</div>
+									<div class="col-auto px-2 mb-3">
+										<img src="<?= base_url('assets/images/formats/betewq5osgluxdan7v5blfsgba.jpg') ?>" alt="" width="120">
+									</div>
+									<div class="col-auto px-2 mb-3">
+										<img src="<?= base_url('assets/images/formats/betewq5osgluxdan7v5blfsgba.jpg') ?>" alt="" width="120">
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div class="d-flex justify-content-between mb-5">
+							<button class="btn btn-outline-dark">Ajouter une nouvelle campagne</button>
+							<button class="btn btn-dark">Terminer</button>
+						</div>
 					</div>
-				</div>
+				</form>
 			</div>
 
 			<div class="col-auto px-3 pt-5">
