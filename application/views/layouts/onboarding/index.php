efb429ee5565a76aaec8a5501d1bc3afe7c449aa
RANDRIANJAFINJOHARY Fandresena Notahiana <fandresena.randrianjafinjohary@cpp-system.com>
2025-10-09 01:17:49 +0300

    Correction : onboarding, title, note, client detail, conge

--- application/views/layouts/onboarding/index.php
+++ application/views/layouts/onboarding/index.php
@@ -52,7 +52,7 @@
 <?php end_section(); ?>
 
 <?php start_section('page_title'); ?>
-Onboarding
+<h1 class="h4 py-2">Onboarding</h1>
 <?php end_section(); ?>
 <?php start_section('page_heading'); ?><?php end_section(); ?>
 
