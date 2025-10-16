bcd0cba13c8848a210235e2e509b8e7554e730e1
RANDRIANJAFINJOHARY Fandresena Notahiana <fandresena.randrianjafinjohary@cpp-system.com>
2025-10-16 17:51:46 +0300

    Finished modify and delete campagne

--- application/views/layouts/client/onboarding/brief-modal.php
+++ application/views/layouts/client/onboarding/brief-modal.php
@@ -1,9 +1,9 @@
 <div class="modal fade" id="briefModal" tabindex="-1" aria-labelledby="briefModalLabel" aria-hidden="true">
-	<form action="<?= empty($d['information_client']) ? base_url("Client/ajout_brief") : "" ?>" enctype="multipart/form-data" method="POST">
+	<form action="<?= base_url("Client/ajout_brief") ?>" enctype="multipart/form-data" method="POST">
 		<div class="modal-dialog modal-dialog-scrollable">
 			<div class="modal-content">
 				<div class="modal-header">
-					<h5 class="modal-title" id="briefModalLabel">Ajouter Brief</h5>
+					<h5 class="modal-title" id="briefModalLabel"><?= empty($d['information_client']) ? "Ajouter Brief" : "Modifier Brief" ?></h5>
 					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 						<span aria-hidden="true">&times;</span>
 					</button>
@@ -12,7 +12,7 @@
 				<div class="modal-body">
 					<div class="form-group">
 						<label for="brief">Brief</label>
-						<textarea name="information_client" id="brief" class="form-control"><?php if (!empty($d['information_client'])) echo $d['information_client']; ?></textarea>
+						<textarea name="information_client" id="brief" rows="10" class="form-control"><?php if (!empty($d['information_client'])) echo $d['information_client']; ?></textarea>
 					</div>
 				</div>
 				<div class="modal-footer">
