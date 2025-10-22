bb94b3f6736aa0c5cc0cdae45b8a5544c2f364ea
RANDRIANJAFINJOHARY Fandresena Notahiana <fandresena.randrianjafinjohary@cpp-system.com>
2025-10-22 17:09:01 +0300

    Basic inventory design

---
+++ application/views/layouts/client/onboarding/modal/client.php
@@ -0,0 +1,15 @@
+<div class="modal fade" id="clientModal" tabindex="-1" role="dialog" aria-labelledby="clientModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-lg" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="clientModalLabel">Détails du Client</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Fermer">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body" id="clientModalContent">
+				Chargement...
+			</div>
+		</div>
+	</div>
+</div>
